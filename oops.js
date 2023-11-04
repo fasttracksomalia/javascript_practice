0e8fac393783fab583167e95336548d116401133
= <=>
2023-11-04 13:59:38 +0000

    objects, classes

--- oops.js
+++ oops.js
@@ -2,14 +2,26 @@
 // player.firstName = "Cristiano"
 // player.lastName = "Ronaldo"
 // player.age = 38
-// console.log(player)
+// //console.log(player.lastName)
 
-//  var player = {
-//     firstName:"Cristiano",
-//     lastName : "Ronaldo",
-//     age : 38
-//  };
-//  console.log(player.lastName)
+// player.shoot = function () {
+//    let lastName = "Waryaa"
+//    console.log(player.firstName + " " + player.lastName + " shot the ball")
+// }
+
+// console.log(player.shoot())
+
+
+// create literal object
+// var player = {
+//    firstName: "Cristiano",
+//    lastName: "Ronaldo",
+//    age: 38,
+//    shoot: function () {
+//       console.log(player.firstName + " " + player.lastName + " shot the ball")
+//    }
+// };
+// console.log(player.shoot())
 
 
 // let player2 = {
@@ -26,63 +38,70 @@
 // }
 
 // player2.yearOfBirth()
-
+// constructor function 
+// before 2015
 // function Player(firstName, lastName, age, team, shirtNumber) {
-//     this.firstName = firstName;
-//     this.lastName = lastName;
-//     this.age = age;
-//     this.team = team;
-//     this.shirtNumber = shirtNumber;
+//    this.firstName = firstName;
+//    this.lastName = lastName;
+//    this.age = age;
+//    this.team = team;
+//    this.shirtNumber = shirtNumber;
 
-//     this.shoot = function () {
-//         console.log(this.firstName + " has shot the ball");
-//     }
+//    this.shoot = function () {
+//       console.log(this.firstName + " has shot the ball");
+//    }
 
 // }
 
-// let player1 = new Player("Cristiano", "Ronaldo", 25, "Al-Nasir", "7")
-// let player2 = new Player("Lionel", "Messi", 25, "Miami", "10")
+// let player1 = new Player("Cristiano", "Ronaldo", 38, "Al-Nasir", "7")
+// let player2 = new Player("Lionel", "Messi", 36, "Miami", "10")
+// let player3 = new Player("Erling", "Halland", 23, "Man City", "9")
 
 // console.log(player1)
 // console.log(player2)
+// console.log(player3)
 
 
 // function Fan(firstName, lastName, age, seatNumber) {
-//     this.firstName = firstName;
-//     this.lastName = lastName;
-//     this.age = age;
-//     this.seatNumber = seatNumber;
+//    this.firstName = firstName;
+//    this.lastName = lastName;
+//    this.age = age;
+//    this.seatNumber = seatNumber;
 
-//     this.waveFlag = function () {
-//         console.log(firstName + " is waving the flag");
-//     }
+//    this.waveFlag = function () {
+//       console.log(firstName + " is waving the flag");
+//    }
 
 // }
+class Fan {
 
-class Fan{
+   constructor(firstName, lastName, age, seatNumber) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.seatNumber = seatNumber;
 
-    constructor(firstName, lastName, age, seatNumber) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.seatNumber = seatNumber;
-     }
-
-    waveFlag() {
-        let firstName = "Waryaa"
-        console.log(firstName + " is waving the flag");
-    }
+   }
+   waveFlag() {
+      let firstName = "Waryaa"
+      console.log(this.firstName + " is waving the flag");
+   }
 
 }
 
-let fan1 = new Fan("Josquare","Lo",33,55)
-let fan2 = new Fan("Abdi","Ali",20,33)
-let fan3 = new Fan("Fatima","Said",19,109)
-//console.log(fan1, fan2, fan3)
-// console.log(fan1.firstName)
-// console.log(fan1['firstName'])
-  let {firstName} = fan1;
-  console.log(firstName)
+let fan1 = new Fan("Jushua", "Lo", 33, 55)
+let fan2 = new Fan("Abdi", "Ali", 20, 33)
+let fan3 = new Fan("Fatima", "Said", 19, 109)
+
+console.log(fan1.waveFlag())
+
+// let { firstName } = fan1; // destructuring Accessor Notation
+// console.log(firstName)
+// console.log(fan1, fan2, fan3)
+// fan1.firstName = "Waryaa"
+//console.log(fan1.firstName) // Dot Accessor Notation
+// console.log(fan1['lastName']) // Square Accessor Notation
+
 
 
 // let fan2 = {
@@ -136,8 +155,10 @@ let fan3 = new Fan("Fatima","Said",19,109)
 
 //console.log(player2)
 
-// let biggerNumber = Math.max(2,5,37,89, 99)
-// console.log(biggerNumber)
+let biggerNumber = Math.max(2,5,37,89, 99)
+console.log(biggerNumber)
+
+// 
 
 
 
